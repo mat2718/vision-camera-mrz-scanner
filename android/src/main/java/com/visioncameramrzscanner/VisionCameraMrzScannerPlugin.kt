34d4f52fab7d7c73b49f215abf2a2c5ee57e922e
Matthew Terry <matthewterry68.mt@gmail.com>
2023-03-19 12:04:35 -0400

    chore(debug): added try catch statements to most functions added debug and error logs to native code for troubleshooting

--- android/src/main/java/com/visioncameramrzscanner/VisionCameraMrzScannerPlugin.kt
+++ android/src/main/java/com/visioncameramrzscanner/VisionCameraMrzScannerPlugin.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.graphics.Point
 import android.graphics.Rect
 import android.media.Image
+import android.util.Log
 import androidx.camera.core.ImageProxy
 import com.facebook.react.bridge.WritableNativeArray
 import com.facebook.react.bridge.WritableNativeMap
@@ -20,82 +21,113 @@ class VisionCameraMrzScannerPlugin: FrameProcessorPlugin("scanMRZ"){
   private fun getBlockArray(blocks: MutableList<Text.TextBlock>): WritableNativeArray {
     val blockArray = WritableNativeArray()
 
-    for (block in blocks) {
-        val blockMap = WritableNativeMap()
-
-        blockMap.putString("text", block.text)
-        blockMap.putArray("recognizedLanguages", getRecognizedLanguages(block.recognizedLanguage))
-        blockMap.putArray("cornerPoints", block.cornerPoints?.let { getCornerPoints(it) })
-        blockMap.putMap("frame", getFrame(block.boundingBox))
-        blockMap.putArray("lines", getLineArray(block.lines))
-
-        blockArray.pushMap(blockMap)
+    try {
+        Log.d("VisionCameraMrzScanner", "blocks: ${blocks.size}")
+        for (block in blocks) {
+            val blockMap = WritableNativeMap()
+    
+            blockMap.putString("text", block.text)
+            blockMap.putArray("recognizedLanguages", getRecognizedLanguages(block.recognizedLanguage))
+            blockMap.putArray("cornerPoints", block.cornerPoints?.let { getCornerPoints(it) })
+            blockMap.putMap("frame", getFrame(block.boundingBox))
+            blockMap.putArray("lines", getLineArray(block.lines))
+    
+            blockArray.pushMap(blockMap)
+        }
+    }
+    catch(e: Exception) {
+        Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
+        e.printStackTrace()
     }
     return blockArray
-} 
+}
+
 
 private fun getLineArray(lines: MutableList<Text.Line>): WritableNativeArray {
     val lineArray = WritableNativeArray()
-
-    for (line in lines) {
-        val lineMap = WritableNativeMap()
-
-        lineMap.putString("text", line.text)
-        lineMap.putArray("recognizedLanguages", getRecognizedLanguages(line.recognizedLanguage))
-        lineMap.putArray("cornerPoints", line.cornerPoints?.let { getCornerPoints(it) })
-        lineMap.putMap("frame", getFrame(line.boundingBox))
-        lineMap.putArray("elements", getElementArray(line.elements))
-
-        lineArray.pushMap(lineMap)
+    try {
+        Log.d("VisionCameraMrzScanner", "lines: ${lines.size}")
+        for (line in lines) {
+            val lineMap = WritableNativeMap()
+    
+            lineMap.putString("text", line.text)
+            lineMap.putArray("recognizedLanguages", getRecognizedLanguages(line.recognizedLanguage))
+            lineMap.putArray("cornerPoints", line.cornerPoints?.let { getCornerPoints(it) })
+            lineMap.putMap("frame", getFrame(line.boundingBox))
+            lineMap.putArray("elements", getElementArray(line.elements))
+    
+            lineArray.pushMap(lineMap)
+        }
+    } catch (e: Exception) {
+        Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
     }
     return lineArray
 }
 
 private fun getElementArray(elements: MutableList<Text.Element>): WritableNativeArray {
     val elementArray = WritableNativeArray()
-
-    for (element in elements) {
-        val elementMap = WritableNativeMap()
-
-        elementMap.putString("text", element.text)
-        elementMap.putArray("cornerPoints", element.cornerPoints?.let { getCornerPoints(it) })
-        elementMap.putMap("frame", getFrame(element.boundingBox))
+    try {
+        Log.d("VisionCameraMrzScanner", "elements: ${elements.size}")
+        for (element in elements) {
+            val elementMap = WritableNativeMap()
+    
+            elementMap.putString("text", element.text)
+            elementMap.putArray("cornerPoints", element.cornerPoints?.let { getCornerPoints(it) })
+            elementMap.putMap("frame", getFrame(element.boundingBox))
+        }
+    } catch (e: Exception) {
+        Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
     }
     return elementArray
 }
 
 private fun getRecognizedLanguages(recognizedLanguage: String): WritableNativeArray {
     val recognizedLanguages = WritableNativeArray()
-    recognizedLanguages.pushString(recognizedLanguage)
+    try {
+        Log.d("VisionCameraMrzScanner", "recognizedLanguage: ${recognizedLanguage}")
+        recognizedLanguages.pushString(recognizedLanguage)
+    } catch (e: Exception) {
+        Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
+    }
     return recognizedLanguages
 }
 
 private fun getCornerPoints(points: Array<Point>): WritableNativeArray {
     val cornerPoints = WritableNativeArray()
-
-    for (point in points) {
-        val pointMap = WritableNativeMap()
-        pointMap.putInt("x", point.x)
-        pointMap.putInt("y", point.y)
-        cornerPoints.pushMap(pointMap)
+    try {
+        Log.d("VisionCameraMrzScanner", "points: ${points.size}")
+        for (point in points) {
+            val pointMap = WritableNativeMap()
+            pointMap.putInt("x", point.x)
+            pointMap.putInt("y", point.y)
+            cornerPoints.pushMap(pointMap)
+        }
+    }
+    catch(e: Exception) {
+        Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
     }
     return cornerPoints
 }
 
 private fun getFrame(boundingBox: Rect?): WritableNativeMap {
     val frame = WritableNativeMap()
-
-    if (boundingBox != null) {
-        frame.putDouble("x", boundingBox.exactCenterX().toDouble())
-        frame.putDouble("y", boundingBox.exactCenterY().toDouble())
-        frame.putInt("top", boundingBox.top)
-        frame.putInt("left", boundingBox.left)
-        frame.putInt("right", boundingBox.right)
-        frame.putInt("bottom", boundingBox.bottom)
-        frame.putInt("width", boundingBox.width())
-        frame.putInt("height", boundingBox.height())
-        frame.putInt("boundingCenterX", boundingBox.centerX())
-        frame.putInt("boundingCenterY", boundingBox.centerY())
+    try {
+        Log.d("VisionCameraMrzScanner", "boundingBox: ${boundingBox}")
+        if (boundingBox != null) {
+            frame.putDouble("x", boundingBox.exactCenterX().toDouble())
+            frame.putDouble("y", boundingBox.exactCenterY().toDouble())
+            frame.putInt("top", boundingBox.top)
+            frame.putInt("left", boundingBox.left)
+            frame.putInt("right", boundingBox.right)
+            frame.putInt("bottom", boundingBox.bottom)
+            frame.putInt("width", boundingBox.width())
+            frame.putInt("height", boundingBox.height())
+            frame.putInt("boundingCenterX", boundingBox.centerX())
+            frame.putInt("boundingCenterY", boundingBox.centerY())
+        }
+    }
+    catch(e: Exception) {
+        Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
     }
     return frame
 }
@@ -103,27 +135,27 @@ private fun getFrame(boundingBox: Rect?): WritableNativeMap {
 @SuppressLint("NewApi")
 override fun callback(frame: ImageProxy, params: Array<Any>): Any? {
 
-    val result = WritableNativeMap()
-
+    val result = WritableNativeMap()    
+    
     val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-
     @SuppressLint("UnsafeOptInUsageError")
     val mediaImage: Image? = frame.getImage()
 
     if (mediaImage != null) {
-        val image = InputImage.fromMediaImage(mediaImage, frame.imageInfo.rotationDegrees)
-        val task: Task<Text> = recognizer.process(image)
-        try {
-            val text: Text = Tasks.await<Text>(task)
-            result.putString("text", text.text)
-            result.putArray("blocks", getBlockArray(text.textBlocks))
+        return try {
+            val image = InputImage.fromMediaImage(mediaImage, frame.imageInfo.rotationDegrees)
+            val task: Task<Text> = recognizer.process(image)
+                val text: Text = Tasks.await<Text>(task)
+                result.putString("text", text.text)
+                result.putArray("blocks", getBlockArray(text.textBlocks))
+                val data = WritableNativeMap()
+                data.putMap("result", result)
+                return data
         } catch (e: Exception) {
-            return null 
+            Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
+            e.printStackTrace()
         }
     }
-
-    val data = WritableNativeMap()
-    data.putMap("result", result)
-    return data
+    return null 
 }
 }
